5eba44cbc2c8cc011c1a09cfb834dc36220b4239
Jason Webb <zen.webb@gmail.com>
2019-08-31 20:43:29 -0500

    Adding initial JS for plugin

---
+++ subtitles.js
@@ -0,0 +1,98 @@
+(function() {
+  var recognition;
+  var final_transcript = '';
+  var start_timestamp;
+  var final_text_el;
+  var interim_text_el;
+
+  setup();
+  start();
+
+  function setup() {
+    if (!('webkitSpeechRecognition' in window)) {
+      console.log('Speech recognition not supported. Use Chrome instead.');
+
+    } else {
+      recognition = new webkitSpeechRecognition();
+      recognition.continuous = true;
+      recognition.interimResults = true;
+
+      recognition.onstart = function() {
+        createHTML();
+      };
+
+      recognition.onerror = function(event) {
+        if (event.error == 'no-speech') {
+          console.log('No speech detected');
+          ignore_onend = true;
+        }
+
+        if (event.error == 'audio-capture') {
+          console.log('No microphone found');
+          ignore_onend = true;
+        }
+
+        if (event.error == 'not-allowed') {
+          if (event.timeStamp - start_timestamp < 100) {
+            console.log('Permission to use microphone is blocked. Change it at chrome://settings/contentExceptions#media-stream');
+          } else {
+            console.log('Permission to use microphone is denied.');
+          }
+
+          ignore_onend = true;
+        }
+      };
+
+      recognition.onresult = function(event) {
+        var interim_transcript = '';
+
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+          if (event.results[i].isFinal) {
+            final_transcript += event.results[i][0].transcript;
+          } else {
+            interim_transcript += event.results[i][0].transcript;
+          }
+        }
+
+        final_transcript = capitalize(final_transcript);
+        final_text.innerHTML = linebreak(final_transcript);
+        interim_text.innerHTML = linebreak(interim_transcript);
+      };
+    }
+  }
+
+  function start() {
+    recognition.start();
+  }
+
+  function stop() {
+    recognition.stop();
+  }
+
+  function createHTML() {
+    var subtitleContainer = document.createElement('div');
+    subtitleContainer.setAttribute('id', 'subtitles');
+
+    final_text_el = document.createElement('span');
+    final_text_el.setAttribute('id', 'final_text');
+
+    interim_text_el = document.createElement('span');
+    interim_text_el.setAttribute('id', 'interim_text');
+
+    subtitleContainer.appendChild(final_text_el);
+    subtitleContainer.appendChild(interim_text_el);
+
+    document.body.appendChild(subtitleContainer);
+  }
+
+  var two_line = /\n\n/g;
+  var one_line = /\n/g;
+  function linebreak(s) {
+    return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+  }
+
+  var first_char = /\S/;
+  function capitalize(s) {
+    return s.replace(first_char, function(m) { return m.toUpperCase(); });
+  }
+})();
